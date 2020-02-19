cb6b538a996aae3ad99f6a97fc3b4a2fa6aee092
Francisco S. Filho <franfellipe2@hotmail.com>
2020-02-19 12:30:00 -0300

    Confirmar dados pedido com popup

--- frontend/fechar-pedido.php
+++ frontend/fechar-pedido.php
@@ -54,7 +54,7 @@
         </table>        
         <br><br>
         <h2>Dados do comprador(a):</h2>
-        <form name="formFecharPedido" action="<?php echo appUrl('/executar-fechar-pedido/'); ?>" method="post">
+        <form id="formFecharPedido" name="formFecharPedido" action="<?php echo appUrl('/executar-fechar-pedido/'); ?>" method="post">
             <p>Os campos com (*) são obrigatórios.</p>
             <div class="form-row">
                 <div class="col-7 mb-3">
@@ -118,26 +118,76 @@
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
-                    <label>Bairro(*):</label>
-                    <input name="bairro" type="text" class="form-control <?php if ($endereco->getError('bairro')) echo 'is-invalid'; ?>"
-                           value="<?php echo $endereco->getBairro(); ?>">
-                    <div class="invalid-feedback">
-                        <?php if ($endereco->getError('bairro')) echo $endereco->getError('bairro'); ?>
-                    </div>
-                </div>
-                <div class="col-md-6 mb-3">
                     <label>Complemento:</label>
                     <input name="complemento" type="text" class="form-control <?php if ($endereco->getError('complemento')) echo 'is-invalid'; ?>"
                            value="<?php echo $endereco->getComplemento(); ?>">
                     <div class="invalid-feedback">
                         <?php if ($endereco->getError('complemento')) echo $endereco->getError('complemento'); ?>
                     </div>
-                </div>                
+                </div>  
+                <div class="col-md-6 mb-3">
+                    <label>Bairro(*):</label>
+                    <input name="bairro" type="text" class="form-control <?php if ($endereco->getError('bairro')) echo 'is-invalid'; ?>"
+                           value="<?php echo $endereco->getBairro(); ?>">
+                    <div class="invalid-feedback">
+                        <?php if ($endereco->getError('bairro')) echo $endereco->getError('bairro'); ?>
+                    </div>
+                </div>                              
             </div>            
             <button class="btn btn-convert btn-lg" type="submit">Finalizar Compra</button>
         </form>
     </div>
 </div>
+<div id="confirmModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog bg-warning" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confira se os dados estão corretos:</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">              
+                <div id="data-confirm"></div>
+            </div>
+            <div class="modal-footer">
+                <button id="btn-form-submit" type="button" class="btn btn-primary">Prosseguir</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Corrigir</button>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require appConfig('frontDir') . '/footer.php'; ?>
+<script type="text/javascript">
+    $('document').ready(function () {
+        $('#formFecharPedido').on('submit', function (e) {
+            e.preventDefault();
+            var form = e.target;
+            var d = {};
+            $(form).serializeArray().forEach(function (v) {
+                d[v.name] = v.value;
+            });
+            var $msg = '<p><strong><i class="far fa-address-card"></i> Dados pessoais:</strong></p>';
+            $msg += '<p>Nome: ' + d.nome + " - CPF: " + d.cpf + "</p>";
+            $msg += '<p>Email: ' + d.email + " - Telefone/Whatssap: " + d.whatssap + "</p>";
+            $msg += "<p><strong><i class=\"fas fa-map-marked-alt\"></i> Endereço para entrega:</strong></p>";
+            $msg += '<p>' + d.longradouro + ', ' + d.numero + " - ";
+            $msg += 'Complemento: ' + d.complemento + "</p>";
+            $msg += '<p>Bairro: ' + d.bairro + ' - Frutal / MG - CEP: 38200-000</p>';
+            $('#data-confirm').html($msg);
+
+            $('#confirmModal').modal('show');
+
+            $('#btn-form-submit').on('click', function () {               
+                form.submit();
+            });
+
+        });
+
+
+
+    });
+
+</script>
 </body>
 </html>
